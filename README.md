2761e6602f7c6db5b8a6f1f4977d4e01c8130a40
anas <anasmj7@gmail.com>
2025-08-22 18:04:00 +0600

    main

---
+++ README.md
@@ -0,0 +1,1 @@
+### Clone the project and run server
--- car_dekho_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from car_dekho_app.models.cars import Cars
-
-# Register your models here.
-admin.site.register(Cars)
