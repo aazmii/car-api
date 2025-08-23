6201c4220c93fb78fad56b5686d950ec29910024
anas <anasmj7@gmail.com>
2025-08-23 19:29:59 +0600

    validations : field, object and func based validator

--- car_dekho_app/api_files/serializers.py
+++ car_dekho_app/api_files/serializers.py
@@ -1,19 +1,42 @@
 from rest_framework import serializers
 
 from car_dekho_app.models.cars import Cars
+def alphanumeric (value): 
+    if not str(value).isalnum():
+        raise serializers.ValidationError('name should be alphanumeric')
+
 class CarSerializier (serializers.Serializer): 
     id = serializers.IntegerField(read_only = True)
     name = serializers.CharField()
     description = serializers.CharField()
     active = serializers.BooleanField(read_only = True)
+    chassisnumber = serializers.CharField(validators = [alphanumeric])
+    price = serializers.DecimalField(max_digits = 9, decimal_places= 2)
 
-    def create (self ,validatedData): 
-        return Cars.objects.create(**validatedData)
+    def create (self ,validated_data): 
+        
+        return Cars.objects.create(**validated_data)
     
-    def update(self, instance,validatedData): 
-        instance.name = validatedData.get('name', instance.name)
-        instance.description = validatedData.get('description', instance.description)
-        instance.active = validatedData.get('active', instance.active)
+    def update(self, instance,validated_data): 
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.active = validated_data.get('active', instance.active)
+        instance.chassisnumber = validated_data.get('chassisnumber', instance.chassisnumber)
+        instance.price = validated_data.get('price', instance.price)
         instance.save()
         return instance
     
+    #FIELD LEVEL VALIDATION
+    def validate_price(self, value): 
+        if value <= 20000: 
+            raise serializers.ValidationError("price should be greater than 20000")
+        return value
+    
+    # OBJECT LEVEL VALIDATOR
+    def validate(self,data): 
+        if data['name'] == data['description']: 
+            raise serializers.ValidationError('Name and description should not be same')
+        return data #must return the data
+        
+    #CUSTOM VALIDATORS
+    # -> SEE alphanumeric
--- car_dekho_app/models/cars.py
+++ car_dekho_app/models/cars.py
@@ -8,6 +8,8 @@ class Cars(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=200)
     active = models.BooleanField(default=False)
+    chassisnumber = models.CharField(max_length = 100, blank = True, null = True)
+    price = models.DecimalField(max_digits=9, decimal_places=2, blank = True, null = True)
     
     def __str__(self):
         return self.name
